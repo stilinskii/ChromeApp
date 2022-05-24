d86385c63b1c02d18caa35320bf9a86c0a32d7c2
stilinskii <lhy1031hy@gmail.com>
2022-05-24 23:06:20 +0900

    add weather

---
+++ js/weather.js
@@ -0,0 +1,18 @@
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEYS}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector('#weather span:first-child');
+      const weather = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("can't find where are you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
